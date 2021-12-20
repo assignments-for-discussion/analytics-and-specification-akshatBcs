3462f6531fcbdd3e76054873081d109c72acfeac
Akshat Sharma <74649838+akshatBcs@users.noreply.github.com>
2021-12-20 20:49:31 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,14 +1,9 @@
-
 function average(numbers) {
   let clean = [];
   for(let i=0; i<numbers.length;i++){
 	if(Number.isInteger(numbers)){
     clean += numbers; 
-    }
-
-
-  
+    } }
   return clean.reduce((p, c)=> p + c, 0) / clean.length;
 }
-
 module.exports = {average};
